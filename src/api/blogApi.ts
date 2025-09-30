0134a04afb98e972799a31d82fe55604a4037dda
graciari2000 <ariane-gracia.akakpo@epitech.eu>
2025-09-30 02:22:11 +0100

    net

--- src/api/blogApi.ts
+++ src/api/blogApi.ts
@@ -1,7 +1,7 @@
 // api/blogApi.ts
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://one00daysbackend.onrender.com/';
 
 export interface Blog {
   _id?: string;
--- src/api/challengeApi.ts
+++ src/api/challengeApi.ts
@@ -1,7 +1,7 @@
 // api/challengeApi.ts
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://one00daysbackend.onrender.com/';
 
 export interface Challenge {
   _id?: string;
